e3f809e36fe735f48b42b4f4abc763e352cead23
Suphachai Sriwised <suphachai.sriwised.o@gmail.com>
2020-11-08 18:36:44 +0700

    intial project BlogMe

---
+++ src/main/java/com/example/BlogMe/BlogMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlogMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BlogMe/BlogMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BlogMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
