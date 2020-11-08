1482a467ae09fa745022cc5a2481adc99a202263
Suphachai Sriwised <suphachai.sriwised.o@gmail.com>
2020-11-08 20:29:53 +0700

    50%

--- src/main/java/com/example/BlogMe/BlogMeApplication.java
+++ src/main/java/com/example/BlogMe/BlogMeApplication.java
@@ -1,7 +1,11 @@
 package com.example.BlogMe;
 
 import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 
 @SpringBootApplication
 public class BlogMeApplication {
